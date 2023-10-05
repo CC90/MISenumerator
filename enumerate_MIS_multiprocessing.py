9e04b4d7c7bca9eec37f1949ffac4b22218aa128
Carl Corea <ccorea@uni-koblenz.de>
2023-10-05 10:16:55 +0200

    ss

--- enumerate_MIS_multiprocessing.py
+++ enumerate_MIS_multiprocessing.py
@@ -146,7 +146,7 @@ def compute_MIS_with_expand_multiprocessing(start_kb,g):
 
 if __name__ == '__main__':
 
-    print(sys.argv)
+    
     file_path = sys.argv[1] if (len(sys.argv)>1) else "mysolver/data/test_small.txt"
 
     # Open the file for reading
@@ -161,7 +161,7 @@ if __name__ == '__main__':
         file_contents = ""
 
     start_kb = file_contents
-    print(start_kb)
+    
 
 
 
